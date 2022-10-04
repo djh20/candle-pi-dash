332933125e135c8ca6cca30c1d1525b9bde6ee78
djh20 <danmanmrdj@gmail.com>
2022-10-04 21:04:56 +1300

    Add text to status icons

--- lib/widgets/status_icon.dart
+++ lib/widgets/status_icon.dart
@@ -4,25 +4,45 @@ class StatusIcon extends StatelessWidget {
   final IconData icon;
   final Color? color;
   final bool active;
-  final double size;
+  final String? activeText;
+  final bool compact;
 
   const StatusIcon({ 
     Key? key,
     required this.icon,
     this.color,
     this.active = true,
-    this.size = 22
+    this.activeText,
+    this.compact = false
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AnimatedOpacity(
-      opacity: active ? 1 : 0.1,
+      opacity: active ? 1 : 0.05,
       duration: const Duration(milliseconds: 500),
-      child: Icon(
-        icon,
-        size: size,
-        color: color,
+      child: Row(
+        children: [
+          Icon(
+            icon,
+            size: 22,
+            color: color
+          ),
+          const SizedBox(width: 3),
+          if (activeText != null)
+            AnimatedOpacity(
+              opacity: (active && !compact) ? 1 : 0,
+              duration: const Duration(milliseconds: 200),
+              child: Text(
+                activeText ?? "",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12,
+                  color: color
+                )
+              )
+            )
+        ],
       ),
     );
   }
