482ad0d040784ccbd06ba7570683b4ed3f7552ab
djh20 <danmanmrdj@gmail.com>
2022-10-05 22:50:47 +1300

    Add notches to power bar

--- lib/widgets/cluster/power_bar.dart
+++ lib/widgets/cluster/power_bar.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:property_change_notifier/property_change_notifier.dart';
 
 const double inMaxPower = 30;
-const double outMaxPower = 85;
-//const double deadZone = 0.015;
+const double outMaxPower = 80;
+const double deadZone = 0.015;
 
 class PowerBar extends StatelessWidget {
   const PowerBar({Key? key}) : super(key: key);
@@ -13,44 +13,87 @@ class PowerBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     final Color contrastingColor = theme.textTheme.bodyText1?.color ?? Colors.black;
-    final Color outColor = contrastingColor.withAlpha(200);
+    final Color outColor = contrastingColor;
     const Color inColor = Color.fromRGBO(30, 212, 51, 1);
 
     return PropertyChangeConsumer<AppModel, String>(
       properties: const ['power_output', 'powered', 'gear'],
       builder: (context, model, properties) {
-        double power = model?.vehicle.getMetricDouble('power_output') ?? 0.0;
+        final double power = model?.vehicle.getMetricDouble('power_output') ?? 0.0;
         final int gear = model?.vehicle.getMetric('gear') ?? 0;
 
         // Vehicle must be in drive or reverse to show power.
         final bool visible = (gear == 4 || gear == 2);
-        if (!visible) power = 0;
+        
+        return AnimatedOpacity(
+          opacity: visible ? 1 : 0,
+          duration: const Duration(milliseconds: 250),
+          child: Row(
+            children: [
+              PowerBarSegment(
+                alignment: Alignment.centerRight,
+                widthFactor: ((-power / inMaxPower) - deadZone).clamp(0, 1),
+                color: inColor,
+                notches: (inMaxPower ~/ 10)
+              ),
 
-        return Row(
-          children: [
-            Expanded(
-              child: FractionallySizedBox(
-                alignment: Alignment.topRight,
-                widthFactor: ((-power / inMaxPower)).clamp(0, 1),
-                child: Container(
-                  color: inColor,
-                  height: 5
-                ),
-              ),
-            ),
-            Expanded(
-              child: FractionallySizedBox(
-                alignment: Alignment.topLeft,
-                widthFactor: ((power / outMaxPower)).clamp(0, 1),
-                child: Container(
-                  color: outColor,
-                  height: 5
-                ),
-              ),
-            )
-          ],
+              const SizedBox(width: 1),
+
+              PowerBarSegment(
+                alignment: Alignment.centerLeft,
+                widthFactor: ((power / outMaxPower) - deadZone).clamp(0, 1),
+                color: outColor,
+                notches: (outMaxPower ~/ 10)
+              )
+            ],
+          ),
         );
       }
+    );
+  }
+}
+
+class PowerBarSegment extends StatelessWidget {
+  final Alignment alignment;
+  final double widthFactor;
+  final Color color;
+  final int notches;
+  final bool hideFirstNotch;
+  
+  const PowerBarSegment({
+    Key? key,
+    required this.alignment,
+    required this.widthFactor,
+    required this.color,
+    this.notches = 0,
+    this.hideFirstNotch = false
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return Expanded(
+      child: Stack(
+        alignment: alignment,
+        children: [
+          FractionallySizedBox(
+            widthFactor: widthFactor,
+            child: Container(
+              color: color,
+              height: 5
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: List.generate(notches, (i) => Container(
+              height: 8,
+              width: 2,
+              color: (i == 0 && hideFirstNotch) ? null : theme.hintColor
+            )),
+          ),
+        ],
+      ),
     );
   }
 }
